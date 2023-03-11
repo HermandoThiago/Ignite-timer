39b2b8df6a9b40423bb21ef5c0d6c5a789b407fc
HermandoThiago <hermandodev@gmail.com>
2023-03-11 14:28:24 -0300

    refactor: context componentization

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react'
 import { HistoryContainer, HisotryList, Status } from './styles'
+import { CyclesContext } from '../../contexts/CyclesContext'
 
 export function History() {
+  const { cycles } = useContext(CyclesContext)
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
+
+      <pre>{JSON.stringify(cycles)}</pre>
 
       <HisotryList>
         <table>
